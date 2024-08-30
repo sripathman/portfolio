4883dda5d9fb563f67550624c914ac9fcff7c2d3
sripathman <sripathmann@gmail.com>
2024-08-30 15:19:09 +0100

    edit and update

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,46 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* PROJECTS SECTION  (Image slider) */
+
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');  
+
+
+let slideIndex = 0;
+
+function showSlide(n)  
+ {
+  if (slides.length === 0) return; // Prevent infinite loop
+
+  slideIndex = (n + slides.length) % slides.length; 
+  // Handle negative and out-of-bounds indices
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+  showSlide(slideIndex  + 1);
+}
+
+prevBtn.addEventListener('click', () => {
+  showSlide(slideIndex - 1);
+});
+
+nextBtn.addEventListener('click', () => {
+  showSlide(slideIndex + 1);
+});
+
+showSlide(slideIndex);  
+
+
+// Auto-slide every 3 seconds
+setInterval(nextSlide, 3000);
